14c55abc2abb0a01e835c779a817f182f69f5f27
Forrest Ben-Jeh Lee <forrest@lnxsrv02.seas.ucla.edu>
2015-04-23 11:13:03 -0700

    all commands preliminarily done except pipe

--- execute-command.c
+++ execute-command.c
@@ -8,11 +8,14 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <fcnl.h>
+#include <fcntl.h>
 
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
+
+// general call for all types of commands   
+void callCommand(command_t c);
 
 int
 command_status (command_t c)
@@ -34,7 +37,7 @@ void setupIO(command_t c)
 			error(2, 0, "io error: dup2 failed");
 		//attempt to close input stream
 		int inp_close = close(inp_fd);
-		if (int_close == -1)
+		if (inp_close == -1)
 			error(2, 0, "io error: unable to close input");
 	}
 	if (c->output != NULL)
@@ -54,13 +57,44 @@ void setupIO(command_t c)
 			error(2, 0, "io error: unable to close output file");
 	}
 }
+void executeANDCommand(command_t c)
+{
+	callCommand(c->u.command[0]);
+	int cStatus1 = command_status(c->u.command[0]);
+	if (cStatus1) 	// if true (fails, returns -1), whole AND command fails, you lose, get a life
+		c->status = cStatus1;
+	else {		// false, first command succeeded, run second command and save status in AND command
+		callCommand(c->u.command[1]);
+		c->status = command_status(c->u.command[1]);
+	}
+}
+
+void executeSequenceCommand(command_t c)
+{
+	callCommand(c->u.command[0]);
+	callCommand(c->u.command[1]);
+	c->status = command_status(c->u.command[0]);
+}
+void executeORCommand(command_t c)
+{
+	callCommand(c->u.command[0]);
+	int cStatus1 = command_status(c->u.command[0]);
+	if (cStatus1) 	// if true (fails, returns -1), try the other command
+	{
+		callCommand(c->u.command[1]);
+		c->status = command_status(c->u.command[1]);
+	}	
+	else {		// false, first command succeeded, can forget about the second command
+		c->status = cStatus1;
+	}
+}
+	
 void executeSimpleCommand(command_t c)
 {
 	pid_t p = fork();
 	//if child process is running, wait till finish, store status
 	if (p > 0)
 	{
-		// for now we assume the child command succeeds
 		int status;
 		waitpid(p, &status, 0);
 		if (!WIFEXITED(status))
@@ -70,15 +104,57 @@ void executeSimpleCommand(command_t c)
 	else if (p == 0)
 	{
 		setupIO(c);
-		
+		execvp(c->u.word[0], c->u.word);
+		// if execvp returns at all, error
+		error (3, 0, "runtime error: %s command failed", c->u.word[0]);
+	}
+	else
+		error(3, 0, "runtime error: forking failed");
+}
+
+void executeSubshellCommand(command_t c)
+{
+	callCommand(c->u.subshell_command);
+	c->status = command_status(c->u.subshell_command);
+}
+
+// general call for all types of commands   
+void callCommand(command_t c)
+{
+	enum command_type type = c->type;
+	switch (type)
+	{
+		case AND_COMMAND:
+			executeANDCommand(c);
+			break;
+		case SEQUENCE_COMMAND:   
+			executeSequenceCommand(c);
+			break;
+		case OR_COMMAND:
+			executeORCommand(c);
+			break;
+		case PIPE_COMMAND:
+			break;
+		case SIMPLE_COMMAND:
+			executeSimpleCommand(c);
+			break;
+		case SUBSHELL_COMMAND:
+			executeSubshellCommand(c);
+			break;
+		default:
+		// should never get here
+			error(3, 0, "runtime error: command not recognized");
+			break;
 	}
 }
+
 void
 execute_command (command_t c, bool time_travel)
 {
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-	 
+	callCommand(c);
+	
 //  error (1, 0, "command execution not yet implemented");
 }
